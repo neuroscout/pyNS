cba1a7a9ab97662d39a02f573b4ffb3aaf95835d
adelavega <delavega@utexas.edu>
2018-08-02 17:38:12 -0500

    Updates to Client API, and update API address

--- pyns/__init__.py
+++ pyns/__init__.py
@@ -6,7 +6,7 @@
     :license: MIT, see LICENSE for more details.
 """
 
-API_BASE_URL = 'https://alpha.neuroscout.org/api/'
+API_BASE_URL = 'http://alpha.neuroscout.org/api/'
 
 from .client import Client
 
