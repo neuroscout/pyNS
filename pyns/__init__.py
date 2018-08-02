e6f7d0ac02251e83db7eae3fd374c9f02ae82e12
adelavega <delavega@utexas.edu>
2018-08-02 16:59:42 -0500

    Add basic project structure, and basic API client

---
+++ pyns/__init__.py
@@ -0,0 +1,16 @@
+"""
+    pyns
+    ~~~~~~~~~~~~~~~~~~~
+    A client for interfacing with http://neuroscout.org API
+    :copyright: (c) 2018 by Alejandro de la Vega.
+    :license: MIT, see LICENSE for more details.
+"""
+
+API_BASE_URL = 'https://alpha.neuroscout.org/api/'
+
+from .client import Client
+
+__all__ = ['Client']
+
+__author__ = ['Alejandro de la Vega']
+__license__ = 'MIT'
