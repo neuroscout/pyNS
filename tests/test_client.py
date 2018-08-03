3f0e41ee9d0cfc31ba6809ba62e5d3ca3f6e0a05
adelavega <delavega@utexas.edu>
2018-08-03 13:12:10 -0500

    Add betamax support for tests, and cassettes of tests

--- tests/test_client.py
+++ tests/test_client.py
@@ -1,11 +1,12 @@
 
-def test_auth(client):
+def test_auth(recorder, client):
     assert client._api_token is not None
     assert len(client._api_token)
 
-def test_datasets(client):
-    resp = client.get('datasets')
-    assert resp.status_code == 200
+def test_datasets(recorder, client):
+    with recorder.use_cassette('get_dataset'):
+        resp = client.get('datasets')
+        assert resp.status_code == 200
 
-    datasets = resp.json()
-    assert len(datasets) > 4
+        datasets = resp.json()
+        assert len(datasets) > 4
