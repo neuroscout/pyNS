9d6d5f79b0e58a9d1776e65093a614de6f5b136a
adelavega <delavega@utexas.edu>
2018-08-11 15:34:17 -0400

    Update docstring

--- pyns/models/base.py
+++ pyns/models/base.py
@@ -8,13 +8,13 @@ class Base(object):
     def parse(cls, data, neuroscout):
         """Return an instance of ``cls`` from ``data``.
         :param data: The structured data.
-        :param reddit: An instance of :class:`.Neuroscout`.
+        :param neuroscout: An instance of :class:`.Neuroscout`.
         """
         return cls(neuroscout, _data=data)
 
     def __init__(self, neuroscout, _data):
         """Initialize a Model instance.
-        :param reddit: An instance of :class:`.Neuroscout`.
+        :param neuroscout: An instance of :class:`.Neuroscout`.
         """
         self._neuroscout = neuroscout
         if _data:
