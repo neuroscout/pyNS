b9e9ce21aa5d1518d73a52e29071f0090581f6d7
adelavega <delavega@utexas.edu>
2018-08-14 11:09:07 -0500

    Remove private user methods

--- pyns/models/user.py
+++ pyns/models/user.py
@@ -4,9 +4,3 @@ from .base import Base
 class User(Base):
     _base_path = 'user'
     _auto_methods = ('get', 'post', 'put')
-
-    def resend_confimation(self):
-        pass
-
-    def reset_password(self):
-        pass
