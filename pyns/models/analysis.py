bd7eb2c4a243de25abf5fb0f3c88a2cd73e49432
adelavega <delavega@utexas.edu>
2018-09-05 16:52:35 -0500

    Add Analysis object that syncs with API

--- pyns/models/analysis.py
+++ pyns/models/analysis.py
@@ -1,17 +1,84 @@
 """Provides the Analysis related classes."""
 from .base import Base
 from pathlib import Path
+from functools import partial
+
+class Analysis:
+    _fields_ = ['dataset_id', 'description', 'name',  'predictions',
+                'predictors', 'private', 'runs']
+    _aliased_methods_ = ['delete', 'bundle', 'compile', 'full', 'resources']
+
+    def __init__(self, *, analyses, name, dataset_id, **kwargs):
+        self.name = name
+        self.dataset_id = dataset_id
+        self._analyses = analyses
+
+        # Set up
+        for k, v in kwargs.items():
+            if k in set(self._fields_):
+                setattr(self, k, v)
+
+        # Create
+        self._fromdict(self._analyses.post(**self._asdict()))
+
+        ## Attach aliased methods
+        for method in self._aliased_methods_:
+            setattr(self,
+                    method,
+                    partial(
+                        getattr(self._analyses, method),
+                        self.hash_id)
+                    )
+
+    def _asdict(self):
+        """ Return dictionary representation of valid fields """
+        di = {}
+        for field in self._fields_:
+            if hasattr(self, field):
+                di[field] = getattr(self, field)
+
+        return di
+
+    def _fromdict(self, di):
+        """ Update field values from response """
+        for k, v in di.items():
+            setattr(self, k, v)
+
+    def push(self):
+        """ Push changes from to API, and sync with returned results"""
+        self._fromdict(self._analyses.put(self.hash_id, **self._asdict()))
+
+    def pull(self):
+        """ Pull updates from API, overriding changes made locally """
+        self._fromdict(self._analyses.get(self.hash_id))
+
+    def get_status(self):
+        """ Get and update compiled status """
+        new_status = self._analyses.status(self.hash_id)
+        self._fromdict(new_status)
+        return new_status
+
+    def clone(self):
+        pass
+
 
 class Analyses(Base):
-    _base_path = 'analyses'
-    _auto_methods = ('get', 'post', 'put')
+    _base_path_ = 'analyses'
+    _auto_methods_ = ('get', 'post')
+
+    def put(self, id, **kwargs):
+        """ Put analysis
+        :param str id: Analysis hash_id.
+        :return: client response object
+        """
+        return self._client._put(self._base_path_, id=id, **kwargs)
 
     def delete(self, id):
         """ Delete analysis
         :param str id: Analysis hash_id.
         :return: client response object
         """
-        return self._client._delete(self._base_path, id=id)
+        return self._client._delete(self._base_path_, id=id)
 
     def bundle(self, id, filename=None):
         """ Get analysis bundle
@@ -34,6 +101,10 @@ class Analyses(Base):
         :return: client response object, with new analysis id
         """
         return self.post(id=id, sub_route='clone')
+
+    def create_analysis(self, *, name, dataset_id, **kwargs):
+        return Analysis(analyses=self, name=name,
+                        dataset_id=dataset_id, **kwargs)
 
     def compile(self, id):
         """ Submit analysis for complilation
