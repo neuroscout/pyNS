0ad34dc73ec7cef03f26bb9a68a07fa5fc9857a7
Alejandro de la Vega <aleph4@gmail.com>
2019-03-11 12:03:53 -0500

    Release 0.2.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setup(name='pyns',
-      version='0.1.3',
+      version='0.2.0',
       description='Neuroscout API wrapper',
       long_description=long_description,
       long_description_content_type="text/markdown",
