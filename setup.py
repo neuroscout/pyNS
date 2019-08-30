978a763a0ca1639bada51722484112599a7264fe
Alejandro de la Vega <aleph4@gmail.com>
2019-08-30 15:24:48 -0500

    Bump to 0.3

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setup(name='pyns',
-      version='0.2.4',
+      version='0.3',
       description='Neuroscout API wrapper',
       long_description=long_description,
       long_description_content_type="text/markdown",
