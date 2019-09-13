e46930eb2cd51ea1a3b9eeb194949a4511b1c369
delavega4 <aleph4@gmail.com>
2019-09-13 16:46:14 -0500

    Add user predictors

--- pyns/models/user.py
+++ pyns/models/user.py
@@ -1,6 +1,14 @@
 """Provides the User class."""
 from .base import Base
 
+
 class User(Base):
-    _base_path_= 'user'
-    _auto_methods_= ('get', 'post', 'put')
+    _base_path_ = 'user'
+    _auto_methods_ = ('get', 'post', 'put')
+
+    def get_predictors(self):
+        """ Get NeuroVault uploads associated with this analysis
+        :param str id: Analysis hash_id.
+        :return: client response object
+        """
+        return self.get(sub_route='predictors')
