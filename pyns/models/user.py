f86ed6b4f7ee1abe7cae71ca9a9a229fd1016153
delavega4 <aleph4@gmail.com>
2019-09-13 17:17:27 -0500

    Add kwargs

--- pyns/models/user.py
+++ pyns/models/user.py
@@ -6,9 +6,9 @@ class User(Base):
     _base_path_ = 'user'
     _auto_methods_ = ('get', 'post', 'put')
 
-    def get_predictors(self):
+    def get_predictors(self, **kwargs):
         """ Get NeuroVault uploads associated with this analysis
         :param str id: Analysis hash_id.
         :return: client response object
         """
-        return self.get(sub_route='predictors')
+        return self.get(sub_route='predictors', **kwargs)
