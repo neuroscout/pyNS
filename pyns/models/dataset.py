d9d83ba34429faa5aee8685372107f356b83b73c
delavega4 <aleph4@gmail.com>
2020-05-05 18:32:44 -0500

    Use get_ consistently. Closes #72

--- pyns/models/dataset.py
+++ pyns/models/dataset.py
@@ -1,14 +1,17 @@
 """Provides the Dataset related classes."""
 from .base import Base
 
+
 class Datasets(Base):
-    _base_path_= 'datasets'
-    _auto_methods_= ('get', )
+    _base_path_ = 'datasets'
+    _auto_methods_ = ('get', )
+
 
 class Tasks(Base):
-    _base_path_= 'tasks'
-    _auto_methods_= ('get', )
+    _base_path_ = 'tasks'
+    _auto_methods_ = ('get', )
+
 
 class Runs(Base):
-    _base_path_= 'runs'
-    _auto_methods_= ('get', )
+    _base_path_ = 'runs'
+    _auto_methods_ = ('get', )
