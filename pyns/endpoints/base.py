05b154a859a5b6b08fa5721da079e9577c08593a
Alejandro de la Vega <aleph4@gmail.com>
2022-12-08 18:43:10 -0600

    If r

--- pyns/endpoints/base.py
+++ pyns/endpoints/base.py
@@ -153,7 +153,7 @@ def _id_to_entities(df):
                 names = {
                     r: endpoint.get(r)['name'] 
                     for r in df[col].unique()
-                    if r is not None
+                    if r
                     }
                 df[col.replace('_id', '_name')] = df[col].map(names)
     return df
